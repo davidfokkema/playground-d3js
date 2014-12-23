8c64bac5dfe9494d203a939d18f6c0b5f7f742af
David Fokkema <davidfokkema@gmail.com>
2014-12-23 19:46:59 +0100

    Two SVG lines alternating

--- test_enter_update_exit/code.js
+++ test_enter_update_exit/code.js
@@ -1,31 +1,42 @@
+var width = 400,
+    height = 300;
+
 var i = 0;
 
-var body = d3.select("body")
+function random_data() {
+  var t = [];
+  for (el in d3.range(Math.random() * 10 + 3)) {
+    t.push(Math.random() * 100);
+  }
+  key = i;
+  i = (i + 1) % 2;
+  return { 'key': key, 'value': t };
+}
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
 
 setInterval(function() {
-  var data = body.selectAll("p")
-      .data([function() {
-        var t = [];
-        for (el in d3.range(Math.random() * 10)) {
-          t.push(Math.random() * 100);
-        }
-        key = i;
-        i = (i + 1) % 2;
-        return { 'key': key, 'value': t };
-      }()], function(d) { return d.key; })
+  var data = svg.selectAll("path")
+      .data([random_data()], function(d) { return d.key; })
 
-  console.log(i);
-
-  data.text("There is no update.");
-  data.enter().append("p")
-      .text(function(d) { return d.value; })
-      .style("opacity", 0)
+  data.enter().append("path")
+      .attr("d", function(d) { return d3.svg.line()
+          .x(function(d, i) { return i * 10; })
+          .y(0)
+        (d.value)})
     .transition()
       .duration(900)
-      .style("opacity", 1);
+      .attr("d", function(d) { return d3.svg.line()
+          .x(function(d, i) { return i * 10; })
+          .y(function(d) { return d; })
+        (d.value)})
+
   data.exit()
     .transition()
-      .duration(900)
+      .duration(500)
       .style("opacity", 0)
       .remove();
 }, 1000);
