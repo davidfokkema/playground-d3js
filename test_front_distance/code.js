579dadc58480cdfa7ab48753bdefc0a1516a3e36
David Fokkema <davidfokkema@gmail.com>
2015-05-11 12:58:43 +0200

    Implement distances to shower front

--- test_front_distance/code.js
+++ test_front_distance/code.js
@@ -38,18 +38,18 @@ var core = g.append("circle")
 var front_rotate_handle = g.append("circle")
     .attr("r", 5)
     .call(drag_alpha);
-update_shower_front();
+
+function front_line_x(fd, dist) { return fd.x + dist * Math.cos(fd.alpha) }
+function front_line_y(fd, dist) { return fd.y + dist * Math.sin(fd.alpha) }
 
 function update_shower_front() {
   var fd = front.datum();
-  function front_line_x(dist) { return fd.x + dist * Math.cos(fd.alpha) }
-  function front_line_y(dist) { return fd.y + dist * Math.sin(fd.alpha) }
 
   front
-      .attr("x1", front_line_x(-FRONT_LENGTH))
-      .attr("y1", front_line_y(-FRONT_LENGTH))
-      .attr("x2", front_line_x(FRONT_LENGTH))
-      .attr("y2", front_line_y(FRONT_LENGTH));
+      .attr("x1", front_line_x(fd, -FRONT_LENGTH))
+      .attr("y1", front_line_y(fd, -FRONT_LENGTH))
+      .attr("x2", front_line_x(fd, FRONT_LENGTH))
+      .attr("y2", front_line_y(fd, FRONT_LENGTH));
 
   core.attr("cx", fd.x)
       .attr("cy", fd.y);
@@ -65,19 +65,32 @@ function update_shower_front() {
   distances
     .attr("x1", function(d) { return d.x; })
     .attr("y1", function(d) { return d.y; })
-    .attr("x2", fd.x)
-    .attr("y2", fd.y);
+    .attr("x2", function(d) { return d.xp; })
+    .attr("y2", function(d) { return d.yp; });
 }
 
 function move_core() {
-  var d = front.datum();
-  d.x = d3.event.x;
-  d.y = d3.event.y;
+  var fd = front.datum();
+  fd.x = d3.event.x;
+  fd.y = d3.event.y;
+  distances.each(calculate_distances);
   update_shower_front();
+}
+
+function calculate_distances(d) {
+  var fd = front.datum();
+  d.r = (d.x - fd.x) * Math.cos(fd.alpha) + (d.y - fd.y) * Math.sin(fd.alpha);
+  d.xp = front_line_x(fd, d.r);
+  d.yp = front_line_y(fd, d.r);
+  d.dist = Math.sqrt((d.x - fd.x)^2 + (d.y - fd.y)^2);
 }
 
 function rotate_front() {
   var d = front.datum();
   d.alpha = Math.atan2(d3.event.y - d.y, d3.event.x - d.x);
+  distances.each(calculate_distances);
   update_shower_front();
 }
+
+distances.each(calculate_distances);
+update_shower_front();
